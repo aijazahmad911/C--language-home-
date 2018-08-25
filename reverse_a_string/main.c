63602d64c917353e2468f7712adddcf6d96719fa
aijazahmad911 <aijazahmad9864@gmail.com>
2018-08-25 12:22:38 +0530

    home

--- reverse_a_string/main.c
+++ reverse_a_string/main.c
@@ -7,7 +7,27 @@ IMCA (SEM-1) */
 #include<string.h>
 int main()
 {
+    int i=0,temp,l=0;
     char a[30];
     printf("enter a string :  ");
     gets(a);
+    l=strlen(a);
+    reverse(l,a);
+    printf("length = %d",l);
+    printf("\nreverse string \n");\
+    for(i=0;i<=l;i++)
+    printf("%c",a[i]);
+    getch();
+
+}
+void reverse(int l,char a[])
+{
+        int i=0,temp;
+        while(i<l){
+        temp=a[i];
+        a[i]=a[l];
+        a[l]=temp;
+        i++;
+        l--;
+    }
 }
