b919c04031b0d8797707fe031d85b033a755fcbd
aijazahmadwani <aijazahmad9864@gmail.com>
2019-08-15 09:58:41 +0530

    total marks

--- Let us C (Vashavant Kanetkar ) problems/chapter 1/F (c)/main.c
+++ Let us C (Vashavant Kanetkar ) problems/chapter 1/F (c)/main.c
@@ -1,8 +1,21 @@
+/*
+Author: Aijaz Ahmad Wani
+email: aijazahmad9864@gmail.com
+Date: 15-8-2019
+*/
+// calculate total marks and percentage
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    float m1,m2,m3,m4,m5,agg_marks,percentage;
+
+    printf("Enter marks in five subjects: ");
+    scanf("%f%f%f%f%f",&m1,&m2,&m3,&m4,&m5);
+    agg_marks = m1+m2+m3+m4+m5;
+    percentage = agg_marks/500*100;
+    printf("\nAggregate Marks = %f ",agg_marks);
+    printf("\nPercentage = %f",percentage);
+
 }
---
+++ Let us C (Vashavant Kanetkar ) problems/chapter 1/F (d)/main.c
@@ -0,0 +1,14 @@
+/*
+Author: Aijaz Ahmad Wani
+email: aijazahmad9864@gmail.com
+Date: 15-8-2019
+*/
+//conversion of temperature
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
