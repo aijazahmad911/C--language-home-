cab825d1bdd2d5f4f1effac2555791a863db8aa1
aijazahmadwani <aijazahmad9864@gmail.com>
2019-08-15 16:40:33 +0530

    calculate BMI

---
+++ Let us C (Vashavant Kanetkar ) problems/chapter 4/D (i)/main.c
@@ -0,0 +1,34 @@
+/*
+Author: Aijaz Ahmad Wani
+email: aijazahmad9864@gmail.com
+Date: 15-8-2019
+*/
+// calculate BMI (Body Mass Index)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float weight,height,BMI;
+    printf("Enter weight in KG: ");
+    scanf("%f",&weight);
+    printf("Enter height in meters: ");
+    scanf("%f",&height);
+    BMI = weight/(height*height);
+
+    printf("BMI = %f",BMI);
+
+    if(BMI<15)
+        printf("Starvation");
+    else if(BMI<17.5)
+        printf("Anorexic");
+    else if(BMI<18.5)
+        printf("Underweight");
+    else if(BMI<24.9)
+        printf("Ideal");
+    else if(BMI<25.9)
+        printf("Overweight");
+    else if(BMI<30.9)
+        printf("Obese");
+    else printf("Morbidly Obese");
+}
