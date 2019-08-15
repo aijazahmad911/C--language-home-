acbbac9f5858dba4c6c281417dd0321607bf2da2
aijazahmadwani <aijazahmad9864@gmail.com>
2019-08-15 17:08:47 +0530

    ASCII characters

---
+++ Let us C (Vashavant Kanetkar ) problems/chapter 5/B (d)/main.c
@@ -0,0 +1,18 @@
+/*
+Author: Aijaz Ahmad Wani
+email: aijazahmad9864@gmail.com
+Date: 15-8-2019
+*/
+// ASCII values and corresponding characters
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0;
+    while(i<=255)
+    {
+        printf("\n%d    %c",i,i);
+        i++;
+    }
+}
