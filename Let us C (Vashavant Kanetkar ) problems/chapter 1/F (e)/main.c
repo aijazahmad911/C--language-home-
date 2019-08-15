cb658b612c91c4cc421bfe4a6cf8bcc1fb766e23
aijazahmadwani <aijazahmad9864@gmail.com>
2019-08-15 10:15:07 +0530

    area and perimeter

--- Let us C (Vashavant Kanetkar ) problems/chapter 1/F (e)/main.c
+++ Let us C (Vashavant Kanetkar ) problems/chapter 1/F (e)/main.c
@@ -1,8 +1,24 @@
+/*
+Author: Aijaz Ahmad Wani
+email: aijazahmad9864@gmail.com
+Date: 15-8-2019
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    float length,breadth,radius;
+    printf("Enter length and breadth of rectangle : ");
+    scanf("%f%f",&length,&breadth);
+
+    printf("Enter Radius of circle: ");
+    scanf("%f",&radius);
+
+    printf("\nArea of rectangle = %f",length*breadth);
+    printf("\nPerimeter of rectangle = %f",(2*(length+breadth)));
+
+
+    printf("\nArea of circle = %f ",(3.14*radius*radius));
+    printf("\nCircumference of circle = %f",2*3.14*radius);
 }
